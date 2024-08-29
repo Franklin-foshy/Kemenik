ccf4c83aae31ba7acf7218090ee252a505fd891b
Franklin-foshy <53231203+Franklin-foshy@users.noreply.github.com>
2024-08-29 00:00:43 -0600

    Update script_nivel1.js
    cambio 2

--- public/assets/js/script_nivel1.js
+++ public/assets/js/script_nivel1.js
@@ -457,7 +457,7 @@ function verificarRespuesta(imagenIndex, preguntaId) {
     let preguntaDiccionario = preguntas[preguntaId - 1];
     let respuestaCorrecta = preguntaDiccionario.correcta;
 
-   /* if (preguntaDiccionario.respuestas[imagenIndex] === respuestaCorrecta) {
+   if (preguntaDiccionario.respuestas[imagenIndex] === respuestaCorrecta) {
         alert("¡Correcto!");
         // Aquí puedes añadir cualquier otra acción en caso de respuesta correcta
     } else {
