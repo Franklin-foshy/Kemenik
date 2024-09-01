7169d608d71951e9d53d32784a548fcfc21fda68
KEVIN01306 <kevin60sanchez31@gmail.com>
2024-09-01 00:57:25 -0600

    config

--- public/assets/js/historia_nivel2.js
+++ public/assets/js/historia_nivel2.js
@@ -21,8 +21,8 @@ let array_opciones = [];
 
 // Cargar las escenas del API
 $.ajax({
-    url: `http://127.0.0.1:8000/api/escenas/`,
-    //url: `https://junamnoj.foxint.tech/api/escenas/`,
+    //url: `http://127.0.0.1:8000/api/escenas/`,
+    url: `https://junamnoj.foxint.tech/api/escenas/`,
     type: 'GET',
     dataType: 'json',
     success: function(sceneResponse) {
@@ -31,8 +31,8 @@ $.ajax({
                 let todo = [] ;
                 // Cargar las preguntas relacionadas con la escena
                 $.ajax({
-                    url: `http://127.0.0.1:8000/api/ppreguntas/`,
-                    //url: `https://junamnoj.foxint.tech/api/ppreguntas/`,
+                    //url: `http://127.0.0.1:8000/api/ppreguntas/`,
+                    url: `https://junamnoj.foxint.tech/api/ppreguntas/`,
                     type: 'GET',
                     dataType: 'json',
                     success: function(questionResponse) {
@@ -45,8 +45,8 @@ $.ajax({
 
                                     // Cargar las respuestas para la pregunta actual
                                     $.ajax({
-                                        url: `http://127.0.0.1:8000/api/prespuestas/`,
-                                        //url: `https://junamnoj.foxint.tech/api/prespuestas/`,
+                                        //url: `http://127.0.0.1:8000/api/prespuestas/`,
+                                        url: `https://junamnoj.foxint.tech/api/prespuestas/`,
                                         type: 'GET',
                                         dataType: 'json',
                                         success: function(response) {
