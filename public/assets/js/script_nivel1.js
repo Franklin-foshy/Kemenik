b35390c585795927644e0b4fb6d4dbb2876523cb
KEVIN01306 <kevin60sanchez31@gmail.com>
2024-09-02 12:46:33 -0600

    arreglo

--- public/assets/js/script_nivel1.js
+++ public/assets/js/script_nivel1.js
@@ -75,7 +75,7 @@ function sendDataToApi(usuario_id, pregunta_id, completado, intentos, puntuacion
     };
 
     // Realizar la solicitud
-    fetch('https://junamnoj.foxint.tech/api/progreso-usuario/', options)
+    fetch('https://junamnoj.foxint.tech/api/progreso-usuario', options)
         .then(response => {
         if (!response.ok) {
             throw new Error('Network response was not ok');
