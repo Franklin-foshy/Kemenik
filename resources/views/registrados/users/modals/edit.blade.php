2a5282ccb124379f850a0272428238148944a051
Ronald David Fuentes <ronaldfuentes.newsoft@gmail.com>
2024-10-03 23:47:31 -0600

    Feature / Validación de actualización en usuarios
    El requerimiento que parecia dificil de aplicar, se analizo y fue desarrollado a la medida a fin de tener algo de mejor calidad

--- resources/views/registrados/users/modals/edit.blade.php
+++ resources/views/registrados/users/modals/edit.blade.php
@@ -44,42 +44,116 @@
                                     <input class="form-control" type="date" name="fecha_nacimiento" placeholder="Escriba la fecha de nacimiento" required value="{{ $user->fecha_nacimiento }}">
                                 </div>
                             </div>
+
+                            <!-- Select País -->
                             <div class="col-md-12 col-lg-12">
                                 <div class="mb-3">
-                                    <label for="pais" class="form-label">País</label>
-                                    <select id="paisEdit" name="pais_id" class="form-control" required>
+                                    <label for="pais{{ $user->id }}" class="form-label">País: <strong>{{ $user->pais->name ?? 'Vacío' }}</strong></label>
+                                    <select id="pais{{ $user->id }}" name="pais_id" class="form-control" required>
                                         <option value="">Selecciona tu país</option>
                                         @foreach($paises as $pais)
-                                        <option value="{{ $pais->id }}" @if ($pais->id == $user->pais_id) selected @endif>{{ $pais->name }}</option>
+                                        <option value="{{ $pais->id }}"
+                                            {{ $user->pais_id == $pais->id ? 'selected' : '' }}>
+                                            {{ $pais->name }}
+                                        </option>
                                         @endforeach
                                     </select>
-                                    <div class="invalid-feedback">
-                                        Por favor selecciona tu país
-                                    </div>
                                 </div>
                             </div>
-                            <div class="col-md-12 col-lg-12" id="departamento-container-edit">
-                                <div class="mb-3">
-                                    <label for="departamento" class="form-label">Departamento</label>
-                                    <select id="departamentoEdit" name="departamento_id" class="form-control">
-                                        <option value="">Selecciona un departamento</option>
+
+                            <!-- Select Departamento -->
+                            <div class="col-md-12 col-lg-12">
+                                <div class="mb-3" id="departamento-container{{ $user->id }}">
+                                    <label for="departamento{{ $user->id }}" class="form-label">Departamento: <strong>{{ $user->departamento->name ?? 'Vacío' }}</strong></label>
+                                    <select id="departamento{{ $user->id }}" name="departamento_id" class="form-control">
+                                        @if($user->pais_id == 185) <!-- Si el país es Guatemala -->
                                         @foreach($departamentos as $departamento)
-                                        <option value="{{ $departamento->id }}" @if ($departamento->id == $user->departamento_id) selected @endif>{{ $departamento->name }}</option>
+                                        <option value="{{ $departamento->id }}"
+                                            {{ $user->departamento_id == $departamento->id ? 'selected' : '' }}>
+                                            {{ $departamento->name }}
+                                        </option>
                                         @endforeach
+                                        @else
+                                        <option value="">Vacío</option>
+                                        @endif
                                     </select>
                                 </div>
                             </div>
-                            <div class="col-md-12 col-lg-12" id="municipio-container-edit">
-                                <div class="mb-3">
-                                    <label for="municipio" class="form-label">Municipio</label>
-                                    <select id="municipioEdit" name="municipio_id" class="form-control">
-                                        <option value="">Selecciona un municipio</option>
+
+                            <!-- Select Municipio -->
+                            <div class="col-md-12 col-lg-12">
+                                <div class="mb-3" id="municipio-container{{ $user->id }}">
+                                    <label for="municipio{{ $user->id }}" class="form-label">Municipio: <strong>{{ $user->municipio->name ?? 'Vacío' }}</strong></label>
+                                    <select id="municipio{{ $user->id }}" name="municipio_id" class="form-control">
+                                        @if($user->departamento_id) <!-- Si hay departamento -->
                                         @foreach($municipios as $municipio)
-                                        <option value="{{ $municipio->id }}" @if ($municipio->id == $user->municipio_id) selected @endif>{{ $municipio->name }}</option>
+                                        <option value="{{ $municipio->id }}"
+                                            {{ $user->municipio_id == $municipio->id ? 'selected' : '' }}>
+                                            {{ $municipio->name }}
+                                        </option>
                                         @endforeach
+                                        @else
+                                        <option value="">Vacío</option>
+                                        @endif
                                     </select>
                                 </div>
                             </div>
+
+                            <script>
+                                document.addEventListener('DOMContentLoaded', function() {
+                                    const paisSelect = document.getElementById('pais{{ $user->id }}');
+                                    const departamentoSelect = document.getElementById('departamento{{ $user->id }}');
+                                    const municipioSelect = document.getElementById('municipio{{ $user->id }}');
+
+                                    if (paisSelect.value == 185) {
+                                        document.getElementById('departamento-container{{ $user->id }}').style.display = 'block';
+                                    }
+
+                                    paisSelect.addEventListener('change', function() {
+                                        const paisId = this.value;
+                                        if (paisId == 185) {
+                                            document.getElementById('departamento-container{{ $user->id }}').style.display = 'block';
+                                            fetch(`/getDepartamentos/${paisId}`)
+                                                .then(response => response.json())
+                                                .then(data => {
+                                                    departamentoSelect.innerHTML = '<option value="">Selecciona un departamento</option>';
+                                                    data.forEach(departamento => {
+                                                        let option = document.createElement('option');
+                                                        option.value = departamento.id;
+                                                        option.text = departamento.name;
+                                                        departamentoSelect.appendChild(option);
+                                                    });
+                                                });
+                                        } else {
+                                            departamentoSelect.innerHTML = '<option value="">Vacío</option>';
+                                            municipioSelect.innerHTML = '<option value="">Vacío</option>';
+                                            document.getElementById('departamento-container{{ $user->id }}').style.display = 'none';
+                                            document.getElementById('municipio-container{{ $user->id }}').style.display = 'none';
+                                        }
+                                    });
+
+                                    departamentoSelect.addEventListener('change', function() {
+                                        const departamentoId = this.value;
+                                        if (departamentoId) {
+                                            document.getElementById('municipio-container{{ $user->id }}').style.display = 'block';
+                                            fetch(`/getMunicipios/${departamentoId}`)
+                                                .then(response => response.json())
+                                                .then(data => {
+                                                    municipioSelect.innerHTML = '<option value="">Selecciona un municipio</option>';
+                                                    data.forEach(municipio => {
+                                                        let option = document.createElement('option');
+                                                        option.value = municipio.id;
+                                                        option.text = municipio.name;
+                                                        municipioSelect.appendChild(option);
+                                                    });
+                                                });
+                                        } else {
+                                            municipioSelect.innerHTML = '<option value="">Vacío</option>';
+                                        }
+                                    });
+                                });
+                            </script>
+
                             <div class="col-md-12 col-lg-12">
                                 <div class="mb-3">
                                     <label class="form-label text-uppercase" for="sexo">Sexo</label>
