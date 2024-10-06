ecdcfdc7ecce46a7c5c9cfcce1f4e9eecbb78e1e
KEVIN01306 <kevin60sanchez31@gmail.com>
2024-10-05 20:58:46 -0600

    actu_api

--- public/assets/js/script_index.js
+++ public/assets/js/script_index.js
@@ -1,3 +1,96 @@
+
+
+
+// consultas de los niveles bloqueo 
+
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    const nivel4Button = document.getElementById('nivel2');
+    const nivel3button = document.getElementById('nivel3');
+
+    // Función para actualizar la accesibilidad del botón Nivel 2
+    function updateCorrectAnswers(count) {
+        if (count === 1) { // Número mínimo de respuestas correctas para desbloquear Nivel 2
+            nivel4Button.style.pointerEvents = 'auto';
+            
+            nivel4Button.style.opacity = 1; // Opcional: hacer que el botón se vea activo
+        } else {
+            nivel4Button.style.pointerEvents = 'none';
+            nivel4Button.style.opacity = 0.5; // Opcional: hacer que el botón se vea desactivado
+        }
+    };
+
+    // Función para actualizar la accesibilidad del botón Nivel 3
+    function updatecoerrec2(count) {
+        let contador = count ;
+        if (contador === 1) {
+            nivel3button.style.pointerEvents = 'auto'; 
+        } else {
+            nivel3button.style.pointerEvents = 'none';
+            nivel3button.style.opacity = 0.5; 
+        };
+    };
+
+
+
+
+
+
+// consultas de los niveles bloqueo 
+let apiUrl = `https://junamnoj.foxint.tech/api/progreso-dos-usuario/${window.userId}`;
+let estado_nivel2 = 0 ; 
+getLastEstadoProceso(apiUrl)
+.then(estadoProceso => {
+    console.log(estadoProceso)
+
+    estado_nivel2 = estadoProceso ;
+    updatecoerrec2(estado_nivel2);
+
+})
+    .catch(error => {
+        console.error('Error:', error);
+    });
+
+let apiUrl1 = `https://junamnoj.foxint.tech/api/progreso-usuario/${window.userId}`;
+let estado_nivel1 = 0 ; 
+    getLastEstadoProceso(apiUrl1)
+    .then(estadoProceso1 => {
+        estado_nivel1 = estadoProceso1 ;
+    console.log(estado_nivel1)
+    updateCorrectAnswers(estado_nivel1);
+    
+})
+    .catch(error => {
+        console.error('Error:', error);
+    });
+    
+    
+
+    // Llamar a la función para actualizar el botón Nivel 2 al cargar la página
+
+    
+    
+    
+    // Llamar a la función para actualizar el botón Nivel 3 al cargar la página
+    
+    
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 /* recojer el proceso de cada nivel */
 function getLastEstadoProceso(url) {
     return new Promise((resolve, reject) => {
@@ -102,107 +195,6 @@ document.addEventListener('DOMContentLoaded', function() {
 
 
 
-// consultas de los niveles bloqueo 
-let apiUrl = `https://junamnoj.foxint.tech/api/progreso-dos-usuario/${window.userId}`;
-let estado_nivel2 = 0 ; 
-getLastEstadoProceso(apiUrl)
-.then(estadoProceso => {
-    console.log(estadoProceso)
-
-    estado_nivel2 = estadoProceso ;
-
-})
-    .catch(error => {
-        console.error('Error:', error);
-    });
-
-let apiUrl1 = `https://junamnoj.foxint.tech/api/progreso-usuario/${window.userId}`;
-let estado_nivel1 = 0 ; 
-    getLastEstadoProceso(apiUrl1)
-    .then(estadoProceso1 => {
-        estado_nivel1 = estadoProceso1 ;
-    console.log(estado_nivel1)
-    
-})
-    .catch(error => {
-        console.error('Error:', error);
-    });
-    
-    
-
-
-// consultas de los niveles bloqueo 
-
-
-
-document.addEventListener('DOMContentLoaded', function() {
-    const nivel4Button = document.getElementById('nivel2');
-    const nivel3button = document.getElementById('nivel3');
-    let completadas_nivel2 = localStorage.getItem('nivel_completado_2');
-
-    let completadas = localStorage.getItem('nivel_completado_1');
-    if (completadas === null) {
-        completadas = 0;
-    } else {
-        completadas = parseInt(completadas, 10); 
-    }
-
-    if (completadas_nivel2 === null) {
-        completadas_nivel2 = 0;
-    } else {
-        completadas_nivel2 = parseInt(completadas_nivel2, 10); 
-    }
-
-
-    // Función para actualizar la accesibilidad del botón Nivel 2
-    function updateCorrectAnswers(count) {
-        if (count === 1) { // Número mínimo de respuestas correctas para desbloquear Nivel 2
-            nivel4Button.style.pointerEvents = 'auto';
-            
-            nivel4Button.style.opacity = 1; // Opcional: hacer que el botón se vea activo
-        } else {
-            nivel4Button.style.pointerEvents = 'none';
-            nivel4Button.style.opacity = 0.5; // Opcional: hacer que el botón se vea desactivado
-        }
-    };
-
-    // Función para actualizar la accesibilidad del botón Nivel 3
-    function updatecoerrec2(count) {
-        let contador = count ;
-        if (contador === 1) {
-            nivel3button.style.pointerEvents = 'auto'; 
-        } else {
-            nivel3button.style.pointerEvents = 'none';
-            nivel3button.style.opacity = 0.5; 
-        };
-    };
-
-    // Función para actualizar la accesibilidad del botón Nivel
-    /*function updatecoerrec2(count) {
-        let contador = count ;
-        // Habilitar el botón de Nivel 3 si se cumplen las condiciones
-        if (contador >= 3) {
-            nivel3button.href = 'c_nivel2.html'; // Cambiar a la URL correcta si es necesario
-            nivel3button.style.pointerEvents = 'auto'; // Habilitar clics
-            nivel3button.style.color = '#000'; // Restaurar color de texto
-        } else {
-            nivel3button.href = 'index.html';
-            nivel3button.style.pointerEvents = 'none'; // Deshabilitar clics
-            nivel3button.style.color = 'gray'; // Estilo de botón deshabilitado
-        };
-    };*/
-
-
-    // Llamar a la función para actualizar el botón Nivel 2 al cargar la página
-
-    updateCorrectAnswers(estado_nivel1);
-    
-    
-    // Llamar a la función para actualizar el botón Nivel 3 al cargar la página
-    updatecoerrec2(estado_nivel2);
-    
-
-});
 
 
 
