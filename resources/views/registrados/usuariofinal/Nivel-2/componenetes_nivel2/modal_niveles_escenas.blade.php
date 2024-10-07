a78810cec7ff297f58eba015ad4e01bf928985b5
Franklin-foshy <53231203+Franklin-foshy@users.noreply.github.com>
2024-10-06 18:02:14 -0600

    Update modal_niveles_escenas.blade.php

--- resources/views/registrados/usuariofinal/Nivel-2/componenetes_nivel2/modal_niveles_escenas.blade.php
+++ resources/views/registrados/usuariofinal/Nivel-2/componenetes_nivel2/modal_niveles_escenas.blade.php
@@ -2,5 +2,5 @@
         <button class="close-btn">&times;</button>
         <img src="{{ asset('imgs/nivel2/niña_hola.gif')}}" alt="Futuristic Image" class="modal-image">
         <h1 class="modal-title" id="modal-title">BIENVENIDO AL NIVEL 2</h1>
-        <p class="modal-text" id="modal-text">Experience the next generation of user interfaces with our cuttfsdfsfsfssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssing-edge designs. The future is now, and it's more exciting than ever.</p>
+        <p class="modal-text" id="modal-text">Hola.</p>
     </div>
