6b7b8d36f1528b13f6d36e0ccf6dea7f9c2a6959
KEVIN01306 <kevin60sanchez31@gmail.com>
2024-10-08 22:35:17 -0600

    commit_arreglos

--- resources/views/registrados/usuariofinal/Nivel-1/componentes_nivel1/modal.blade.php
+++ resources/views/registrados/usuariofinal/Nivel-1/componentes_nivel1/modal.blade.php
@@ -5,18 +5,6 @@
         <span class="close" style="display: none">&times;</span>
         <h2 id="pregunta">{{_('')}}</h2>
         <div class="cont_imagenes_modal" id="contenedor_modal_imagenes">
-            <!--<div class="contenedor_de_preguntas_respuestas">
-                <img src="{{ asset('') }}" class="respuesta" id="imagen1" alt="Respuesta 1" data-id="0">
-                <p>{{_('Hola')}}</p>
-            </div>            
-            <div class="contenedor_de_preguntas_respuestas">
-                <img src="{{ asset('') }}" class="respuesta" id="imagen2" alt="Respuesta 2" data-id="1">
-                <p>{{_('djsadkdlñkdsalsdklsdksakñaskdldkkdksadlñk')}}</p>
-            </div>
-            <div class="contenedor_de_preguntas_respuestas">
-                <img src="{{ asset('') }}" class="respuesta" id="imagen3" alt="Respuesta 3" data-id="2">
-                <p>{{_('jsadsjdosjdopdjdsajdospajspdajsoapsdopajdspojsadopjsadpo')}}</p>
-            </div>-->
         </div>
         <div class="modal-continuar" id="modal-continuar" style="display: none">
             {{_('PARA SEGUIR AVANZANDO PRIMERO TIENES QUE ARMAR EL ROMPECABEZAS Y RESPONDER TODAS LAS PREGUNTAS.')}}
